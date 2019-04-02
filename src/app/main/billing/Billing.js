3554e1bfc7d9e43c77eb154c96bf1f2fd2c9b1f8
reza julian <reza.julian@ifca.co.id>
2019-04-02 11:41:51 +0700

    billing

--- src/app/main/billing/Billing.js
+++ src/app/main/billing/Billing.js
@@ -1,16 +1,34 @@
 import React, {Component} from 'react';
 import {FusePageCarded} from '@fuse';
-import {withStyles, List, ListItem, ListItemText } from '@material-ui/core';
+import {withStyles, Typography, List, ListItem, Tab, Tabs, Card, CardContent, CardHeader  } from '@material-ui/core';
 
 const styles = theme => ({
-    layoutRoot: {}
+    layoutRoot: {},
+    card: {
+        minWidth: 275,
+        marginLeft: -30,
+        marginRight: -30,
+        marginTop: -30,
+        marginBottom: 20,
+    },
 });
 
+
 class Billing extends Component {
+
+    state = {
+        value: 0
+    };
+
+    handleChange = (event, value) => {
+        this.setState({value});
+    };
 
     render()
     {
         const {classes} = this.props;
+        const {value} = this.state;
+
         return (
             <FusePageCarded
                 classes={{
@@ -20,24 +38,82 @@ class Billing extends Component {
                     <div className="p-24"><h1>Billing Info</h1></div>
                 }
                 contentToolbar={
-                    <div className="px-24"><h4>Content Toolbar</h4></div>
+                    <Tabs
+                        value={value}
+                        onChange={this.handleChange}
+                        textColor="primary"
+                        variant="fullWidth"
+                        scrollButtons="off"
+                        className="w-full h-64"
+                    >
+                        <Tab className="h-64" label="Due"/>
+                        <Tab className="h-64" label="Current"/>
+                    </Tabs>
                 }
                 content={
                     <div className="p-24">
-                        <List component="nav">
-                            <ListItem button>
-                                <h1>Test 1</h1>
-                            </ListItem>
-                            <ListItem button>
-                                <h1>Test 1</h1>
-                            </ListItem>
-                            <ListItem button>
-                                <h1>Test 1</h1>
-                            </ListItem>
-                            <ListItem button>
-                                <h1>Test 1</h1>
-                            </ListItem>
-                        </List>
+                        {value === 0 &&
+                        (
+                            <List component="nav">
+                                <ListItem>
+                                    <Card className={classes.card}>
+                                        <CardHeader
+                                            subheader="IFCA Tower Office"
+                                        />
+                                        <CardContent>
+                                            <Typography component="p">
+                                                This impressive paella is a perfect party dish and a fun meal to cook together with your
+                                                guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>
+                                </ListItem>
+                                <ListItem>
+                                    <Card className={classes.card}>
+                                        <CardHeader
+                                            subheader="IFCA Tower Office"
+                                        />
+                                        <CardContent>
+                                            <Typography component="p">
+                                                This impressive paella is a perfect party dish and a fun meal to cook together with your
+                                                guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>
+                                </ListItem>
+                                <ListItem>
+                                    <Card className={classes.card}>
+                                        <CardHeader
+                                            subheader="IFCA Tower Office"
+                                        />
+                                        <CardContent>
+                                            <Typography component="p">
+                                                This impressive paella is a perfect party dish and a fun meal to cook together with your
+                                                guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>
+                                </ListItem>
+                                <ListItem>
+                                    <Card className={classes.card}>
+                                        <CardHeader
+                                            subheader="IFCA Tower Office"
+                                        />
+                                        <CardContent>
+                                            <Typography component="p">
+                                                This impressive paella is a perfect party dish and a fun meal to cook together with your
+                                                guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>
+                                </ListItem>
+                            </List>
+                        )}
+                        {value === 1 && (
+                            <div>
+                                <h3 className="mb-16">Item Two</h3>
+                            </div>
+                        )}
                     </div>
                 }
             />
